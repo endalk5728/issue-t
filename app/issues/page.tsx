1fcfceafa45ba4e16843d202c90ffe8a1eba1cfd
endalk5728 <endalkengida8@gmail.com>
2024-01-01 01:07:48 -0800

    issues status badge

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 
 
 import { PrismaClient } from '@prisma/client'
+import IssueStatusbadge from '../component/IssueStatusbadge';
 
 const prisma = new PrismaClient()
 
@@ -32,10 +33,12 @@ const Issuespage = async() => {
 
 <Table.Cell>
   {issue.title}
-<div className='block md:hidden'>{issue.status}</div>
+<div className='block md:hidden'>
+  <IssueStatusbadge status={issue.status}/>
+</div>
 <div  className='text-blue-800 block md:hidden'>{issue.createdAt.toDateString()}</div>
 </Table.Cell>
-<Table.Cell className='hidden md:table-cell'>{issue.status}</Table.Cell>
+<Table.Cell className='hidden md:table-cell'><IssueStatusbadge status={issue.status}/></Table.Cell>
 <Table.Cell className='hidden md:table-cell'>{issue.createdAt.toDateString()}</Table.Cell>
       </Table.Row>
 
